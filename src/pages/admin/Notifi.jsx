f3ec3d6a43673b9423b7bbd0852ee59fdfa0dfdc
Vivianne Christi <61090532+VivianneChristi@users.noreply.github.com>
2025-02-13 15:01:13 -0300

    75%

--- src/pages/admin/Notifi.jsx
+++ src/pages/admin/Notifi.jsx
@@ -1,109 +1,134 @@
-import React from 'react';
-import './Notifi.css'; //importação do CSS
-import Sidebar from '../../components/SiderBar/Siderbar'
+import React from "react";
+import "./Notifi.css"; //importação do CSS
+import Sidebar from "../../components/SiderBar/Siderbar";
 import { AiOutlineExclamationCircle } from "react-icons/ai";
+import { IoMdExit } from "react-icons/io";
 
 const notifications = [
-    {
-        type: "Solicitação de livro",
-        title: "Harry Potter e as Relíquias da Morte",
-        author: "J.K. ROWLING",
-        urgency: null,
-    },
-    {
-        type: "Solicitação de Auxílio em evento",
-        requestedBy: "Richard Augusto",
-        event: "Sarau Cultural",
-        specialNeed: "Deficiência visual",
-        date: "18/10/2025",
-        urgency: "urgent",
-    },
-    {
-        type: "Solicitação de Auxílio em evento",
-        requestedBy: "Caique Soares",
-        event: "Sessão teatro",
-        specialNeed: "Deficiência física (Pessoa com cadeira de rodas)",
-        date: "20/12/2025",
-        urgency: "moderate",
-    },
-    {
-        type: "Solicitação de Auxílio em evento",
-        requestedBy: "Sandra Castilho",
-        event: "Sessão cinema",
-        specialNeed: "Deficiência auditiva (Surdez total)",
-        date: "10/01/2026",
-        urgency: "low",
-    }
+  {
+    type: "Solicitação de livro",
+    title: "Harry Potter e as Relíquias da Morte",
+    author: "J.K. ROWLING",
+    urgency: null,
+  },
+  {
+    type: "Solicitação de Auxílio em evento",
+    requestedBy: "Richard Augusto",
+    event: "Sarau Cultural",
+    specialNeed: "Deficiência visual",
+    date: "18/10/2025",
+    urgency: "urgent",
+  },
+  {
+    type: "Solicitação de Auxílio em evento",
+    requestedBy: "Caique Soares",
+    event: "Sessão teatro",
+    specialNeed: "Deficiência física (Pessoa com cadeira de rodas)",
+    date: "20/12/2025",
+    urgency: "moderate",
+  },
+  {
+    type: "Solicitação de Auxílio em evento",
+    requestedBy: "Sandra Castilho",
+    event: "Sessão cinema",
+    specialNeed: "Deficiência auditiva (Surdez total)",
+    date: "10/01/2026",
+    urgency: "low",
+  },
 ];
 
 const urgencyIcons = {
-    urgent: <AiOutlineExclamationCircle className="icon urgent" />,
-    moderate: <AiOutlineExclamationCircle className="icon moderate" />,
-    low: <AiOutlineExclamationCircle className="icon low" />
+  urgent: <AiOutlineExclamationCircle className="icon" style={{backgroundColor:'#b92922'}} />,
+  moderate: <AiOutlineExclamationCircle className="icon" style={{backgroundColor:'#de8b1c'}} />,
+  low: <AiOutlineExclamationCircle className="icon" style={{backgroundColor:'#3bab64'}} />,
 };
 
 function Notif() {
+  return (
+    <div className="lateral">
+      <Sidebar />
+      <container className="background">
+        <div className="container-pai">
+          <div className="navBar">
+            <h2>
+              Bom dia, <b>Samantha!</b>
+            </h2>
+            <p>
+              Voce tem 1{" "}
+              <a href="#" style={{ color: "#00bf15" }}>
+                notificação
+              </a>
+            </p>
+            <IoMdExit />
+          </div>
+          <hr style={{ width: "101%", margin: "0px auto", }} />
+          <div id="titulo">
+            <h1>
+              Notificações
+              <hr style={{ width: "101%"}} />
+            </h1>
+          </div>
 
-    return (
-        <div className="lateral">
-            <Sidebar />
-            <container className="background">
+          <div className="notif-container">
+            <div className="notifc-content">
+              <h2>Solicitação de livro</h2>
+              <p>Título: Harry Potter e as reliquidas da morte</p>
+              <p>Autor(a): J.K ROWLING</p>
+            </div>
+          </div>
 
-                <div className='container-pai'>
+          <div className="notifications">
+            {notifications.map((notification, index) => (
+              <div key={index} className="notif-container">
+                
+                <div className="notifc-content">
+                {notification.urgency && (
+                  <div className="notif-nivel">
+                    {urgencyIcons[notification.urgency]}
+                  </div>
+                )}
+                  <h2>{notification.type}</h2>
+                  {notification.title && <p>Título: {notification.title}</p>}
+                  {notification.author && (
+                    <p>Autor(a): {notification.author}</p>
+                  )}
+                  {notification.requestedBy && (
+                    <p>Solicitado por: {notification.requestedBy}</p>
+                  )}
+                  {notification.event && <p>Evento: {notification.event}</p>}
+                  {notification.specialNeed && (
+                    <p>Necessidade especial: {notification.specialNeed}</p>
+                  )}
+                  {notification.date && <p>Data: {notification.date}</p>}
 
-                    <div className='navBar'>
-
-                        <h2>Bom dia, <b>Samantha!</b></h2>
-                        <p>Voce tem 1 <a href='#' style={{ color: '#00bf15', }} >notificação</a></p>
-                        <p>icone de saida</p>
-
-                    </div>
-                    <hr style={{ width: '100%', margin: '0px auto', }} />
-                    <div id="titulo">
-                        <h1>Notificações<hr style={{ width: '200%', margin: '0px auto', }} /></h1>
-                    </div>
-
-                    <div className='notif-container'>
-                        <div className="notifc-content">
-                            <h2>Solicitação de livro</h2>
-                            <p>Título: Harry Potter e as reliquidas da morte</p>
-                            <p>Autor(a): J.K ROWLING</p>
-                        </div>
-                    </div>
-
-                    <div className="notifications">
-                        {notifications.map((notification, index) => (
-                            <div key={index} className='notif-container'>
-                                <div className="notifc-content">
-                                    <h2>{notification.type}</h2>
-                                    {notification.title && <p>Título: {notification.title}</p>}
-                                    {notification.author && <p>Autor(a): {notification.author}</p>}
-                                    {notification.requestedBy && <p>Solicitado por: {notification.requestedBy}</p>}
-                                    {notification.event && <p>Evento: {notification.event}</p>}
-                                    {notification.specialNeed && <p>Necessidade especial: {notification.specialNeed}</p>}
-                                    {notification.date && <p>Data: {notification.date}</p>}
-                                </div>
-
-                                {notification.urgency && (
-                                    <div className="notif-nivel">
-                                        {urgencyIcons[notification.urgency]}
-                                    </div>
-                                )}
-                            </div>
-                        ))}
-                    </div>
-
-                    <div className="nivel">
-                        <p> <AiOutlineExclamationCircle id='ExU' /> Urgente (Prazo expira em menos de 15 dias)</p>
-                        <p> <AiOutlineExclamationCircle id='ExA' /> Atenção moderada (Prazo expira em um mês)</p>
-                        <p><AiOutlineExclamationCircle id='ExS' /> Sem urgência (prazo expira em um mês e meio ou mais)</p>
-                    </div>
-
+                  
                 </div>
 
-            </container>
+                
+              </div>
+            ))}
+          </div>
+
+          <div className="nivel">
+            <p>
+              {" "}
+              <AiOutlineExclamationCircle size={40} id="ExU" /> Urgente (Prazo expira em
+              menos de 15 dias)
+            </p>
+            <p>
+              {" "}
+              <AiOutlineExclamationCircle size={40} id="ExA" /> Atenção moderada (Prazo
+              expira em um mês)
+            </p>
+            <p>
+              <AiOutlineExclamationCircle size={40} id="ExS" /> Sem urgência (prazo expira
+              em um mês e meio ou mais)
+            </p>
+          </div>
         </div>
-    );
+      </container>
+    </div>
+  );
 }
 
 export default Notif;
